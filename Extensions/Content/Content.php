b13ed86baf0b84e66f5a580aefc2d14f3bf3fe26
Denis Glushkov <amkarovec@gmail.com>
2022-11-19 15:40:11 +0500

    Base Component Content moved to Core. + Added content templates

--- Extensions/Content/Content.php
+++ Extensions/Content/Content.php
@@ -13,61 +13,6 @@ use Simplex\Core\Page;
  * Output content on site page
  *
  */
-class Content extends \Simplex\Core\ComponentBase
+class Content extends \Simplex\Extensions\Content\Content
 {
-
-    public function &get()
-    {
-        $q = "SELECT * FROM content WHERE active=1 AND path=@SITE_PATH";
-        if ($content = DB::result($q)) {
-            $content['params'] = unserialize($content['params']);
-        }
-        return $content;
-    }
-
-    public static function getStatic($alias)
-    {
-        $q = "SELECT * FROM content WHERE active=1 AND alias = '$alias'";
-        if ($content = DB::result($q)) {
-            $content['params'] = unserialize($content['params']);
-        }
-        return $content;
-    }
-
-    protected function content()
-    {
-        $content = $this->get();
-
-        if ($content) {
-            if (!Core::ajax()) {
-                $this->breadcrumbs($content);
-            }
-
-            Page::seo($content['title']);
-
-            $children = array();
-            if (empty($content['params']['hide_children'])) {
-                $q = "SELECT content_id, title, short, text, path, photo FROM content WHERE active=1 AND pid=" . (int)$content['content_id'];
-                $children = DB::assoc($q);
-            }
-            include dirname(__FILE__) . '/tpl/com_item.tpl';
-        } else {
-            Core::error404();
-        }
-    }
-
-    private function breadcrumbs($content)
-    {
-        Breadcrumbs::add($content['title'], $content['path']);
-        $id = (int)$content['pid'];
-        while ($id) {
-            $q = "SELECT pid, title, path FROM content WHERE content_id=$id";
-            $id = 0;
-            if ($content = DB::result($q)) {
-                Breadcrumbs::add($content['title'], $content['path']);
-                $id = (int)$content['pid'];
-            }
-        }
-    }
-
 }
